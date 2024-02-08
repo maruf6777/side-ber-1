42bb794e2ebf4281e83e69c0c4ba3d0164dac907
maruf6777 <marufahmedsumon436@gmail.com>
2024-02-08 12:36:38 +0600

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+const sideber = document.querySelector('.sideber')
+
+const toggleBtn = document.querySelector('.toggle-btn')
+
+toggleBtn.addEventListener('click', ()  =>  {sideber.classList.toggle('active');});
